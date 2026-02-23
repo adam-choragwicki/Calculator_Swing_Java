208d6c0cbf69a493dd08c4dc98049e4d3827be10
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2026-02-23 18:06:06 +0100

    Make the Calculator run in EDT

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,9 +1,11 @@
 package calculator;
 
+import javax.swing.SwingUtilities;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        new Calculator();
+        SwingUtilities.invokeLater(Calculator::new); // run in Event Dispatch Thread
     }
 }
